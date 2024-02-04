1b0d702b7dbe473bd877730545afda8d5a7ecb81
Alex Myers <amyers3000@outlook.com>
2024-02-04 17:41:41 -0600

    Initial Commit

---
+++ CSE 503 Week 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSE 503 Week 4
+//
+//  Created by Alex Myers on 2/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
